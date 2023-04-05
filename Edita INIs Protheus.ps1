0568fde82219ac0a013dfafd44d98455d023f096
tialianca <57809637+tialianca@users.noreply.github.com>
2023-04-05 12:15:50 -0300

    Commit inicial

---
+++ Edita INIs Protheus.ps1
@@ -0,0 +1,3 @@
+﻿
+Get-ChildItem s:\protheus12\bin*\appserver* -Recurse -Filter appserver*.ini | Foreach-Object{& 'C:\Program Files (x86)\Notepad++\notepad++.exe' $_.FullName}
+#Get-ChildItem s:\protheus12\bin*\appserver* -Recurse -Filter console.log | Foreach-Object{& 'C:\Program Files (x86)\Notepad++\notepad++.exe' $_.FullName}
